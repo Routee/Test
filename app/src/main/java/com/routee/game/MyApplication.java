7281e4ebf4b6070504ccf6245ba8984daff41335
Routee <atfir@qq.com>
2018-04-20 15:45:34 +0800

    Initial commit

---
+++ app/src/main/java/com/routee/game/MyApplication.java
@@ -0,0 +1,15 @@
+package com.routee.game;
+
+import android.app.Application;
+
+/**
+ * @author: Routee
+ * @date 2018/4/20
+ * @mail wangc4@qianbaocard.com
+ * ------------1.本类由Routee开发,阅读、修改时请勿随意修改代码排版格式后提交到git。
+ * ------------2.阅读本类时，发现不合理请及时指正.
+ * ------------3.如需在本类内部进行修改,请先联系Routee,若未经同意修改此类后造成损失本人概不负责。
+ */
+
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/com/routee/game/module/main/GameActivity.java
@@ -0,0 +1,12 @@
+package com.routee.game.module.main;
+
+import com.routee.game.R;
+import com.routee.game.base.BaseActivity;
+
+public class GameActivity extends BaseActivity {
+
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_main);
+    }
+}
